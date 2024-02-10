d469dff218ec46c98b792b0efa1cdc30cef0abc7
IvanDirigible <sickeruncible@gmail.com>
2024-02-10 13:39:33 -0500

    Renamed note_id to id. Now it matches html code. GetOne and delete work now.

--- routes/api/noteRoutes.js
+++ routes/api/noteRoutes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
-const { readFromFile, readAndAppend } = require('../../helpers/noteUtils');
+const { readFromFile, readAndAppend, writeToFile } = require('../../helpers/noteUtils');
 const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the notes
 router.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+// GET Route for a specific note
+router.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
 
 // POST Route for a new note
 router.post('/', (req, res) => {
@@ -17,7 +30,7 @@ router.post('/', (req, res) => {
     const newNote = {
       title,
       text,
-      note_id: uuidv4(),
+      id: uuidv4(),
     };
 
     readAndAppend(newNote, './db/db.json');
@@ -28,16 +41,17 @@ router.post('/', (req, res) => {
 });
 
 // DELETE Route for a specific note
-router.delete('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
-    readFromFile('./db/db/notes.json')
+router.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
       .then((data) => JSON.parse(data))
       .then((json) => {
+
         // Make a new array of all notes except the one with the ID provided in the URL
-        const result = json.filter((note) => note.note_id !== noteId);
+        const result = json.filter((note) => note.id !== noteId);
   
         // Save that array to the filesystem
-        writeToFile('./db/db/notes.json', result);
+        writeToFile('./db/db.json', result);
   
         // Respond to the DELETE request
         res.json(`Item ${noteId} has been deleted 🗑️`);
